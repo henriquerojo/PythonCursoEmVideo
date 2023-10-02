0884e770b5ccba23c6b03165d3ccf7e942330f79
henriquerojo <henriquerojo10@gmail.com>
2023-10-02 20:41:11 -0300

    finalização exercicio 5 utilizando loop FOR para obter o indice dos vetores

---
+++ listaJupiter/ex005.py
@@ -0,0 +1,9 @@
+#5. Calcule o lucro mensal (use um loop for). Arredonde para 2 casas decimais usando round()
+
+receitas = [14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50]
+despesas = [12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96]
+meses=['jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez']
+tamanho = len(receitas)
+for c in range(0, tamanho):
+    lucro = receitas[c] - despesas[c]
+    print(f'No mês {meses[c]} o lucro foi de {round(lucro, 2)}')
