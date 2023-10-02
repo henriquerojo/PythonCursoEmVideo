7fe690945621d0e851db65fa8fdadd6a8607ac45
henriquerojo <henriquerojo10@gmail.com>
2023-10-02 17:43:14 -0300

    finalizando exercício 1 para criar uma soma de todos os números pares entre 1 e 100 utilizando a estrutura FOR

---
+++ listaJupiter/ex001.py
@@ -0,0 +1,6 @@
+# 1. Escreva um programa que calcule a soma de todos os números pares de 1 a 100 e imprima o resultado.
+
+soma = 0
+for i in range(2, 101, 2):
+    soma += i
+print(f'A soma de todos os números pares entre 1 e 100 é de {soma}')
